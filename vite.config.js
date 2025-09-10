062f20dbb109394f4fea251fdb6141f81a5a8ce7
Reyys21 <Reyhanhan096@example.com>
2025-09-10 21:22:35 +0800

    Update project absensi

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,10 @@ import laravel from "laravel-vite-plugin";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
+    build: {
+        outDir: "public/build", // pastikan hasil build masuk ke sini
+        emptyOutDir: true,
+    },
     plugins: [
         laravel({
             input: ["resources/css/app.css", "resources/js/app.js"],
